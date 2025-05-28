fa5d4f8f8ff6c09fa827bacbcdabbcd4bfcfede8
Heitorvazeg <heitorvazeg@gmail.com>
2025-05-28 13:32:34 -0300

    ✨ feat: Navbar nas páginas; ajustes responsividade navbar

--- frontend/shared/navbar.js
+++ frontend/shared/navbar.js
@@ -3,25 +3,30 @@ const template = `
         <style>
         .navbar {
             position: fixed;
+            z-index: 100;
             top: 0;
             left: 0;
             width: 4rem;
             height: 100%;
             background: linear-gradient(90deg, hsl(0, 12%, 16%), hsl(0, 13%, 26%), hsl(0, 16%, 19%));
             transition: ease 0.3s;
+            font-family; "Inria Serif", "Times New Roman";
 
             &:hover {
-                width: 12rem;
+                width: 10rem;
             }
 
             &:hover .navegacao {
                 display: flex;
+                opacity: 1;
+                transition:
+                    opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1);
             }
 
             &:hover .icone {
                 width: 4rem;
                 height: 4rem;
-                padding: 2rem;
+                padding: 1rem;
                 margin-bottom: 2rem;
                 margin-left: 3rem;
             }
@@ -36,36 +41,79 @@ const template = `
         }
 
         .navegacao {
-            display: none;
             flex-direction: column;
             align-items: center;
             justify-content: center;
             width: 100%;
+            opacity: 0;
+            transition:
+                opacity 0.10s cubic-bezier(0.4, 0, 0.2, 1);
         }
 
-        .navegacao a {
+        .navegacao a {            
             width: 100%;
-            height: 4rem;
+            height: 3rem;
             color: whitesmoke;
             text-decoration: none;
-            font-size: 1.2rem;
+            font-size: 1rem;
             display: flex;
             align-items: center;
+            padding: 1rem;
 
             &:hover {
                 cursor: pointer;
                 background-color: hsl(0, 11%, 39%);
             }
         }
+        
+        @media (min-width: 1366px) {
+            .navbar {
+                width: 6rem;
+
+                &:hover {
+                    width: 12rem;
+                }
+            }
+
+            .navegacao a {
+                font-size: 1.3rem;
+                height: 4rem;
+            }
+        }
+
+        @media (max-width: 1000px) {
+            .navbar {
+                width: 2rem;
+
+                &:hover {
+                    width: 12rem;
+                }
+            } 
+        }
+
+        @media (max-width: 700px) {
+            .navbar {
+                &:hover {
+                    width: 10rem;
+                }
+            }
+        }
+
+        @media (max-width: 560px) {
+            .navbar {
+                width: 1.5rem;
+            }
+        }
+
     </style>
     <nav class="navbar">
         <div class="icone">
             <img src="" alt="ICONE">
         </div>
         <div class="navegacao">
-            <a href="./inicioPagina.html">Início</a>
+            <a href="../inicioPagina/inicioPagina.html">Início</a>
             <a href="../formularioPaciente/FormularioPaciente.html">Configurações</a>
-            <a href="">Ajuda</a>
+            <a href="../centralAjuda/centralAjuda.html">Ajuda</a>
             <a href="">Sair</a>
         </div>        
     </nav>
